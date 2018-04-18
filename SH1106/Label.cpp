43ea7f523ff7b93e4686be566c3c2ac3c3c87944
noearchimede <noearchimede@gmail.com>
2018-04-18 23:42:46 +0200

    End of label class rewriting, beginning tests

--- SH1106/Label.cpp
+++ SH1106/Label.cpp
@@ -3,6 +3,8 @@
 #include "Label.hpp"
 
 
+
+
 Label::Label(uint8_t width, uint8_t height, uint8_t startCol, uint8_t startPage)
 :
 // Make the frame two bytes less wide to allow a one-pixel margin on each side
@@ -12,72 +14,235 @@ cursor(frame),
 
 }
 
-void Label::moveCursor(uint8_t column, uint8_t page) {
 
+
+
+void Label::clear() {
+    fill();
 }
 
-void Label::space() {
 
+
+
+bool Label::write(char text[]) {
+    uint16_t i = 0;
+    while(text[i++]);
+    return write(text, i);
 }
 
-void Label::tab() {
 
+
+
+bool Label::write(char text[], uint16_t length) {
+
+    uint16_t wordStartIndex = 0;
+    bool charsToPrint = false;
+
+    uint16_t i = 0;
+    while(i < length) {
+        bool escapeSequence;
+        MoveType move;
+        move = getMoveChar(escapeSequence, text[i], (i+1 < length? text[i+1] : '\0'));
+
+        if(move != MoveType::none) {
+            // Print last word
+            if(charsToPrint) {
+                charsToPrint = false;
+                if(!writeWord(text, wordStartIndex, i)) return false;
+            }
+
+            // Perform required action on cursor
+            switch(move) {
+                case MoveType::space:
+                if(!space()) return false;
+                break;
+                case MoveType::tab:
+                if(!tab()) return false;
+                break;
+                case MoveType::newline:
+                if(!newline()) return false;
+                break;
+                case MoveType::carriageReturn:
+                if(!carriageReturnClear()) return false;
+                break;
+            }
+        }
+        // else we know that the character has to be interpreted as text
+        else {
+            // If this is the first printable character after a space, tab, ...
+            if(!charsToPrint) {
+                wordStartIndex = i;
+                charsToPrint = true;
+            }
+        }
+
+        // The only possible escapes here are the "action" ones (e.g. "\\t").
+        // The other are analyzed while measuring and printing a word.
+        if(escapeSequence) i++;
+        i++;
+    }
+    return true;
 }
+
+
+
+
+
+
+void Label::tab(uint8_t anchor) {
+    constexpr uint8_t defaultTab = 25;
+
+    // calculate tab width
+    uint8_t tab;
+    // user-defined anchor
+    if(anchor) {
+        if(cursor.column < anchor) {
+            tab = anchor - cursor.column;
+        }
+        else {
+            // The cursor is already on the right side of the anchor.
+            // Just write a space to make sure that the previous word will not
+            // be joined up with the next one.
+            space();
+            return;
+        }
+    }
+    // default tab
+    else tab = defaultTab - (cursor.column % defaultTab);
+
+    // Clear the space jumped by the tab
+    if(frame.isInFrame(cursor.column + tab))
+    fill(0x00, cursor.column, cursor.page, (cursor.column + tab), cursor.page);
+    else
+    fill(0x00, cursor.column, cursor.page, 0xFF, cursor.page);
+
+    // move the cursor
+    cursor.move(tab, true);
+}
+
+
+
 
 bool Label::newline() {
-
-}
-
-void Label::clear(uint8_t beginCol, uint8_t beginPag, uint8_t endCol, uint8_t endPag) {
-
-}
-
-bool Label::write(char *text) {
-
-}
-
-bool Label::write(char *text, uint16_t length) {
-
-}
-
-
-void Label::writeArray(uint8_t column, uint8_t page, uint8_t data [], uint8_t length) {
-
+    cursor.page++;
+    if(frame.isInFrame(cursor.column, curosor.page)) return true;
+    return false;
 }
 
 
 
-// ##### PRIVATE ##### ///
 
-PageFont::Char Label::readString(bool& escaped, MoveType& move, char char1, char char2) {
-    // "Custom" escape character used in this class. Could be a backslash
-    // (write it twice - '\\' - in string literals to tell the compiler to
-    // escape it) but also a less used character like '#' or '@'. Don't use
-    // backtick as it is used as accent.
-    // The escape character can be written by typing it twice without spacing
-    // ('\\\\' or '##').
-    constexpr char escape = '\\'; // backslash
+bool Label::carriageReturnClear() {
+    fill(0x0,0,cursor.page,0xFF,cursor.page);
+    // `fill` moves the cursor to the `begin` position.
+}
+
+
+
+
+void Label::space() {
+    if(frame.isInFrame(cursor.column + getAsciiCharWidth(' ')))
+    writeChar(font.getAscii(' '));
+}
+
+
+
+
+
+
+bool Label::moveCursor(uint8_t column, uint8_t page) {
+    if(frame.isInFrame(column, page)) {
+        cursor.column = column;
+        cursor.page = page;
+        return true;
+    }
+    return false;
+}
+
+
+
+
+
+
+void Label::fill(char data, uint8_t beginCol, uint8_t beginPag, uint8_t endCol, uint8_t endPag) {
+
+    // if one of the end parameters is 0xff replace it with its max value
+    if(endPag == 0xFF) endPag = frame.pages - 1;
+    if(endCol == 0xFF) endCol = frame.columns - 1;
+
+    // if the range is "negative" return
+    if(endPag < beginPag) return;
+    else if(endPag == beginPag) {
+        if(endCol < startCol) return;
+    }
+
+    // clear (part of) the first line
+    cursor.page = beginPag;
+    for(int i = beginCol; i < frame.columns; i++) {
+        cursor.column = i;
+        driver.writeData(frame.absolutePage(cursor.page), frame.absoluteColumn(cursor.column), data);
+    }
+    // clear intermediate line(s)
+    for(int k = beginPag + 1; k < endPag; k++) {
+        cursor.page = k;
+        for(int i = 0; i < frame.columns; i++) {
+            cursor.column = i;
+            driver.writeData(frame.absolutePage(cursor.page), frame.absoluteColumn(cursor.column), data);
+        }
+    }
+    // clear (part of) the last line
+    cursor.setPage(endPag);
+    for(int i = 0; i <= endCol; i++) {
+        cursor.column = i;
+        driver.writeData(frame.absolutePage(cursor.page), frame.absoluteColumn(cursor.column), data);
+    }
+
+    cursor.page = beginPag;
+    cursor.column = beginCol;
+}
+
+
+
+
+bool Label::writeArray(uint8_t column, uint8_t page, uint8_t data [], uint8_t length) {
+
+    // cut the array, if needed, to fit in current page
+    bool cut = false;
+    if(!frame.isInFrame(cursor.column + length)) {
+        lenght = frame.columns - cursor.column;
+        cut = true;
+    }
+
+    driver.writeData(frame.absolutePage(column), frame.absoluteColumn(page), data, length);
+
+    cursor.move(length);
+
+    return !cut;
+}
+
+
+
+
+
+// ################################ PRIVATE ################################# //
+
+
+
+
+PageFont::Char Label::getPrintableChar(bool& bothUsed, char char1, char char2) {
+
 
     // Default return values, will be changed according to the situation.
     PageFont::Char c = font.getUnknown();
-    move = MoveType::none;
-    escaped = true;
+    bothUsed = true;
 
 
     // Here begins a series of if - elseif conditions that looks for every
     // possible escape sequence. The else condition at the end is only reached
     // if the given text must simply be treated as plain text.
 
-    // "Action" characters escaped with the custom escape character
-    if(char1 == escape) {
-        // The returned character will be a 'unknown' symbol.
-        if(char2 == 'n') move = MoveType::newline;
-        if(char2 == 't') move = MoveType::tab;
-        if(char2 == 'r') move = MoveType::carriageReturn;
-    }
-
     // accented vowels
-    else if(char2 == '`') {
+    if(char2 == '`') {
         if     (char1 == 'a') c = font.getAccentedVowel('a');
         else if(char1 == 'e') c = font.getAccentedVowel('e');
         else if(char1 == 'i') c = font.getAccentedVowel('i');
@@ -96,16 +261,23 @@ PageFont::Char Label::readString(bool& escaped, MoveType& move, char char1, char
     else if(char1 == '^'    && char2 == '-') c = font.getArrow(7); // ^-
 
     // Digits at exponent position
-    else if(char1 == '^' && ('0' <= char2 && char2 >= '9') c = font.getExpDigit(char2 - '0')
+    else if(char1 == '^' && ('0' <= char2 && char2 >= '9')) c = font.getExpDigit(char2 - '0');
 
     // Special characters
     // degree symbol
-    else if((char1 == escape || char1 == '^' && char2 == 'o') c = font.getSpecialChar(PageFont::SpecialChar::degree);
+    else if((char1 == escape || char1 == '^') && char2 == 'o') c = font.getSpecialChar(PageFont::SpecialChar::degree);
     // Copyright symbol
     else if(char1 == escape && char2 == 'c') c = font.getSpecialChar(PageFont::SpecialChar::copyright);
 
     // Double escape character
     else if(char1 == escape && char2 == escape) c = font.getAscii(escape);
+
+    // "Action" characters escaped with the custom escape character are treated
+    // by another function and will be considered as 'unknown' characters.
+    // Thus this function should not be called if the character or character
+    // sequence is a tab, newline or carriage return. Neither should it be
+    // called for spaces, but if it were it would return a fixed width space.
+    else if (char1 == escape && (char2 == 'n' || char2 == 't' || char2 == 'r')) c = font.getUnknown();
 
     // Unused escape character. It is ignored as may be used to avoid another
     // escape sequence: e.g. "->" prints a right arrow, "-\\>" prints "->",
@@ -118,62 +290,125 @@ PageFont::Char Label::readString(bool& escaped, MoveType& move, char char1, char
     // ### NO SPECIAL CHARACTER SEQUENCE ### //
 
     else {
-        // Some special ASCII characters. The returned character is 'unknown'.
-        // They are treated as their "custom-escaped" counterparts.
-        if(char1 == '\n') move = MoveType::newline;
-        else if(char1 == '\t') move = MoveType::tab;
-        else if(char1 == '\r') move = MoveType::carriageReturn;
-        // space (ASCII 0x20), treated as a special character to allow automatic
-        // line wrap if the text width exceeds the label width
-        else if(char1 == ' ') move = MoveType::space;
-        // plain ASCII text (from '!' to '~')
-        else if(0x21 <= char1 && char1 <= 0x7E) c = font.getAscii(char1);
-        // unknown ASCII character
-        else c = font.getUnknown;
-
-        // RETURN
-        escaped = false;
+        // plain ASCII text (will be a real character from ASCII '!' to '~',
+        // i.e. 0x20 to 0x7E, and a printable 'unknown' character otherwise)
+        c = font.getAscii(char1);
+        // Update default return value
+        bothUsed = false;
     }
 
-    // ### SPECIAL CHARACTER SEQUENCE FOUND ### //
-
-    //RETURN
     return c;
 }
 
 
 
 
+Label::MoveType Label::getMoveChar(bool& bothUsed, char char1, char char2) {
 
+    MoveType move = MoveType::none;
+    bothUsed = false;
 
-
-
-
-
-
-
-
-
-uint16_t Label::getWordWidth(char word [], uint16_t charCount) {
-    PageFont::Char c;
-    int i = 0;
-    while(i < charCount) {
-
+    if(char1 == ' ') move = MoveType::space;
+    else if(char1 == escape) {
+        if(char2 == 'n') { move = MoveType::newline; bothUsed = true; }
+        if(char2 == 't') { move = MoveType::tab; bothUsed = true; }
+        if(char2 == 'r') { move = MoveType::carriageReturn; bothUsed = true; }
     }
+    else if(char1 == '\n') move = MoveType::newline;
+    else if(char1 == '\t') move = MoveType::tab;
+    else if(char1 == '\r') move = MoveType::carriageReturn;
+
+    return move;
 }
 
-bool Label::writeSingleChar(PageFont::Char c) {
 
+
+
+uint16_t Label::getWordWidth(char word[], uint16_t firstIndex, uint16_t stopIndex) {
+    // All characters are assumed to be printable
+    uint16_t width = 0;
+    uint16_t i = firstIndex;
+    while(i < stopIndex) {
+        // get the character that should be printed
+        PageFont::Char c;
+        bool escapeSequence;
+        c = getPrintableChar(escapeSequence, word[i], (i < stopIndex ? word[i+1] : '\0'));
+        // get its width
+        width += c.length;
+        // Increase `i` by one or two if two characters were joined together
+        if(escapeSequence) i++;
+        i++;
+    }
+    return width;
 }
+
+
+
+
+uint8_t Label::getAsciiCharWidth(char character) {
+    PageFont::Char c;
+    c = font.getAscii(character);
+    return c.length;
+}
+
+
+
+
+bool Label::writeWord(char word[], uint16_t firstIndex, uint16_t stopIndex) {
+    // All characters passed to this function are assumed to be printable.
+    // If some non printable characters are fond they will be print as
+    // "unknown"
+
+    // get word width
+    const uint16_t width = getWordWidth(text, wordStartIndex, i);
+    // check whether it is possible to print it and move the cursor if needed
+    if(!cursor.prepare(width)) return false;
+
+    uint16_t i = firstIndex;
+    while(i < stopIndex) {
+        // Get the printable character
+        PageFont::Char c;
+        bool escapeSequence;
+        c = getPrintableChar(escapeSequence, word[i], (i < stopIndex ? word[i+1] : '\0'));
+
+        if(!writeChar(c)) return false;
+
+        // Go to next character
+        if(escapeSequence) i++;
+        i++;
+    }
+    return true;
+}
+
+
+
 
 bool Label::writeChar(PageFont::Char c) {
 
+    // This check shouldn't normally be necessary because other functions
+    // should call `cursor.prepare() before calling this function, but it could
+    // yield a positiver result (resulting in this function returning false) in
+    // some special cases, for example if a word is wider than a line of the
+    // label.
+    if(!frame.isInFrame(cursor.column, cursor.page)) return false;
+
+    // Print character
+    driver.writeData(frame.absolutePage(cursor.page), frame.absoluteColumn(cursor.column), c.data, c.length);
+    if(!cursor.move(c.length)) return false;
+    // Print one pixel space
+    driver.writeData(cursor.page, cursor.column, 0x0);
+    if(!cursor.move(1)) return false;
+
+    return true;
 }
+
 
 
 
 
 // ### Frame ### //
+
+
 
 Label::Frame::Frame(uint8_t width, uint8_t height, uint8_t xPos, uint8_t yPos) {
     // Dimension
@@ -184,30 +419,60 @@ Label::Frame::Frame(uint8_t width, uint8_t height, uint8_t xPos, uint8_t yPos) {
     pagShift = yPos;
 }
 
-uint8_t Label::Frame::absoluteColumn(uint8_t relativeColumn) {
-    return relativeColumn + colShift;
+
+
+Label::Frame::isInFrame(uint8_t relativeColumn, uint8_t relativePage) {
+    return (relativeColumn < columns && relativePage < pages);
 }
 
-uint8_t Label::Frame::absolutePage(uint8_t relativePage) {
-    return relativePage + pagShift;
+
+
+// In case of invalid parameters, the first line/column of the label may be
+// overwritten. This is preferred to overwriting other parts of the screen and
+// shows that this class is somewhere ill-formed.
+uint8_t Label::Frame::absoluteColumn(uint8_t relativeColumn) {
+    if(relativeColumn < columns)
+    return relativeColumn + colShift;
+    else
+    return colShift;
 }
+
+
+uint8_t Label::Frame::absolutePage(uint8_t relativePage) {
+    if(relativePage < pages)
+    return relativePage + pagShift;
+    else
+    return pagShift;
+}
+
+
 
 
 
 // ### Cursor ### //
+
+
 
 Label::Cursor::Cursor(Frame& frame) :
 frame(frame)
 {
     column = 0;
     page = 0;
-    outOfFrame = false;
 }
 
+
+
 bool Label::Cursor::prepare(uint8_t textWidth) {
+
     // there is enough space starting from current location, do nothing
     if(column + textWidth < frame.columns) return true;
-    // there isn't, try to move to next line
+    // there isn't, try to move to next line but before doing it check if the
+    // word would fit in an empty line.
+    if(textWidth > columns) {
+        // the word is too long and needs to be wrapped at least once.
+        // It's characters will be written until reacing the end of the label.
+        return true;
+    }
     if(page + 1 < frame.pages) {
         // ok, there is another available line
         page++;
@@ -217,6 +482,8 @@ bool Label::Cursor::prepare(uint8_t textWidth) {
     // no available page, don't move and return false
     return false;
 }
+
+
 
 bool Label::Cursor::move(uint8_t steps, bool stopAtNL) {
     // if needed go to another line
--- SH1106/Label.hpp
+++ SH1106/Label.hpp
@@ -89,46 +89,17 @@ public:
     //! Consturctor - provides label size and position
     Label(uint8_t width, uint8_t height, uint8_t startCol, uint8_t startPage);
 
-    //! Move the cursor to a given location
-    void moveCursor(uint8_t column, uint8_t page);
-
-    //! Write a space
-    /*! The space will overwrite any previouvsly written characters, but it
-    will be cut if it is at end of a line.
-    */
-    void space();
-
-    //! Write a tab
-    /*! By default there is an anchor every 25 pixels. Those anchors are used
-        when there is no paramenter or the 'anchor' parameter is 0.
-        It is however possible to set a custom anchor defined in distance from
-        the left (i.e. 60 will make the cursor jump to column 60). If the
-        cursor is already placed right of the anchor the tab will draw a space.
-
-        This function actually writes some empty bytes, i.e. it will overwrite
-        any underlying pixel.
-    */
-    void tab();
-
-    //! Go to the next line
-    /*! @return `false` if there isn't any additional line below the current one
-    */
-    bool newline();
-
-    //! Go to the beginning of current line and clear line
-    void carriageReturnClear();
-
-    //! Clear the label
-    /*! The cursor will be moved to (0,0)
-    void clear();
-
-    //! Clear all characters written between two points
-    /*! The cursor will be moved to the "begin" location.
-        The "begin" and "end" points are given in the form (beginX, beginY, endX, endY) */
-    void clear(uint8_t beginCol, uint8_t beginPag, uint8_t endCol, uint8_t endPag);
 
     //! Prints a NULL-TERMINATED string literal or char array on the screen
-    /*! The text is encoded in ASCII.
+    /*! The text is encoded in ASCII but can contain a number of special
+        sequences, listed in the class description. It will be print starting
+        from the current cursor position and will be cut at the end of the
+        label. Words (sequences of characters beetween two spaces) will not be
+        split between two lines, if there isn't enough space for a word on the
+        current line the cursor will automatically move to the next one, but
+        spacing will not be influenced by the amount of free space at the end
+        of the line.
+
         @warning The char array must end with a '\0' (NULL) character.
                  '\0' is automatically appended to string literals.
 
@@ -139,45 +110,159 @@ public:
     */
     bool write(char text[]);
     //! Prints a char array of given lenght on the screen.
-    /*! @param text A char array containing the text to print. NULL characters
+    /*! @see write(char text[])
+        @param text A char array containing the text to print. NULL characters
                     ('\0') will be ignored.
         @param length Lengt of the text array.
     */
     bool write(char text[], uint16_t length);
 
+    //! Clear the label
+    /*! The cursor will be moved to (0,0)
+    */
+    void clear();
+
+
+    //! Write a tab
+    /*! By default there is an anchor every 25 pixels. Those anchors are used
+        when there is no paramenter or the 'anchor' parameter is 0.
+        It is also possible to set a custom anchor defined as a distance from
+        the left (i.e. 60 will make the cursor jump to column 60). If the
+        cursor is at a column at the right side of the anchor only a space will
+        be drawn.
+
+        This function actually writes some empty bytes, i.e. it will overwrite
+        any underlying pixel.
+    */
+    void tab(uint8_t anchor = 0);
+
+    //! Go to the next line
+    /*! @return `false` if there isn't any additional line below the current one
+    */
+    bool newline();
+
+    //! Go to the beginning of current line and clear line
+    void carriageReturnClear();
+
+    //! Write a space
+    /*! The space will overwrite any previouvsly written characters, but it
+    will be cut if it is at end of a line.
+    */
+    void space();
+
+
+    //! Move the cursor to a given location
+    /*! If the given position is out of the label the cursor will not be moved
+        and this function will return false.
+    */
+    bool moveCursor(uint8_t column, uint8_t page);
+
+    //! Write a single byte on a given part of the label
+    /*! The cursor will be moved to the "begin" location.
+        The "begin" and "end" points are given in the form (beginX, beginY, endX, endY).
+        `data` is a byte that will be repeatedly printed.
+        If the end position is seto to 0xff it will be changed to the end of the
+        label.
+
+        If called with the default arguments this function clears the entire
+        label (i.e. writes 0s on it).
+
+        After execution the cursor is moved to the `begin` position.
+    */
+    void fill(char data = 0x00, uint8_t beginCol = 0, uint8_t beginPag = 0, uint8_t endCol = 0xFF, uint8_t endPag = 0xFF);
+
+
     //! Draw a number of consecutive bytes on a page as if it were a character
-    /*! Draw any number of bytes on a single page
+    /*! Draw any number of bytes on a single page. If the array is wider than
+        the remaining space on the current page only a part of it will be
+        printed, i.e. the printing of an array will not continue on the next
+        page as it would in the case of a string.
+        The printing will begin at current cursor positio and the cursor will
+        be moved to the end of the string.
+
         @param page     Frame page on wich data will be displayed
         @param column   Frame column of the first data byte
         @param data     Array of bytes to be displayed
         @param length   Length of the `data` array
+
+        @return This function will only return `true` if the array has been
+                _entirely_ print. This means that it could return `false` while
+                some bytes were printed.
+
     */
     void writeArray(uint8_t column, uint8_t page, uint8_t data[], uint8_t length);
 
 
+
 private:
 
-    // This enum is used in one parameter of the `readString` function
+
+    // ### SETTINGS ### //
+
+    // "Custom" escape character used in this class. Could be a backslash
+    // (write it twice - '\\' - in string literals to tell the compiler to
+    // escape it) but also a less used character like '#' or '@'. Don't use
+    // backtick as it is used as accent.
+    // The escape character can be printed by typing it twice without spacing
+    // ('\\\\' or '##').
+    constexpr char escape = '\\'; // backslash
+
+
+    // ### FUNCTIONS ### //
+
+    // This enum is used as return type of the `getMoveChar()` function
     enum class MoveType {none, space, tab, newline, carriageReturn};
+    // Check whether `char1` or the char1-char2 sequence are an "action"
+    // character (newline, tab, carriage return, space), i.e. if they require
+    // this class to move the cursor.
+    // Retruns MoveType::none if not.
+    // If the characters were an escaped sequence the variable given as first
+    // parameter will be `true`
+    // The second character may be NULL while the first one should be a
+    // printable character or one of '\n', '\t' and '\r'
+    MoveType getMoveChar(bool& bothUsed, char char1, char char2 = '\0');
 
     // Function used to read a characher array wich may contain escaped
-    // characters. If the parameter `escaped` is true the two characters were
+    // characters. If the parameter `bothUsed` is true the two characters were
     // joined to produce a single output (e.g. 'e' and '`' will result in 'è').
-    // If the character is a tab, newline or space the `move` parameter will
-    // have a non-zero value according to the above enum.
-    geFont::Char readString(bool& escaped, MoveType& move, char char1, char char2)
+    // The second character may be NULL while the first one should be a
+    // printable character.
+    //
+    // "Action" characters escaped with the custom escape character are treated
+    // by another function and will be considered as 'unknown' characters here.
+    // Thus this function should not be called if the character or character
+    // sequence is a tab, newline or carriage return. Neither should it be
+    // called for spaces, but if it were it would return a fixed width space.
+    geFont::Char getPrintableChar(bool& bothUsed, char char1, char char2 = '\0');
 
-    // Get the width in pixels of a word with charCount characters
-    uint16_t getWordWidth(char word[], uint16_t charCount);
 
-    // Write one single character (used in the single character writing functions)
-    bool writeSingleChar(PageFont::Char c);
+    // Get the width in pixels of a word with charCount characters.
+    // The string must not contain newlines, tabs or carriage returns and
+    // should not contain spaces.
+    // Only lements [firstIndex] to [stopIndex - 1] of the array will be
+    // considered.
+    uint16_t getWordWidth(char word[], uint16_t firstIndex, uint16_t stopIndex);
+
+    // Get the width of any printable ASCII character (including space)
+    uint8_t getAsciiCharWidth(char character);
+
+    // Write a given number of printable characters. The string must not contain
+    // newlines, tabs or carriage returns and should not contain spaces
+    // Only lements [firstIndex] to [stopIndex - 1] of the array will be
+    // considered.
+    bool writeWord(char word[], uint16_t firstIndex, uint16_t stopIndex);
 
     // Write a character
-    bool writeChar(PageFont::Char c);
+    void writeChar(PageFont::Char c);
 
+    // ### VARIABLES ###
+
+
+    // Reference to an instance of the display driver
+    SH1106_driver & driver;
     // Instance of the class used to render characters
     PageFont font;
+
 
     // Information about the frame of the label
     struct Frame {
@@ -187,19 +272,21 @@ private:
         uint8_t colShift, pagShift;
         // Dimension of the frame
         uint8_t columns, pages;
+        // Check whether a given RELATIVE column/page address is in the label
+        // The page may be omitted and will then be assumed to be in frame.
+        bool isInFrame(uint8_t relativeColumn, uint8_t relativePage = 0);
         // Functions to convert a relative address to an absolute one
         uint8_t absoluteColumn (uint8_t relativeColumn);
         uint8_t absolutePage (uint8_t relativePage);
     };
     Frame frame;
 
+
     // Text writing cursor
     struct Cursor {
         Cursor(Label::Frame & frame);
         // Current cursor position
         uint8_t column, page;
-        // Set to true when the cursor reaches the end of the label
-        bool outOfFrame;
         // Put the cursor on the next available location to write a given
         // number of pixels, i.e. either leave it where it is either move it
         // to next line. Returns false if there isn't space enough.
@@ -208,7 +295,6 @@ private:
         // By default the cursor will not move further after moving to next line
         // returns false if the cursor goes out of bounds
         bool move(uint8_t steps, bool stopAtNL = true);
-
     private:
         // Reference to the frame struct of the enclosing class
         Label::Frame & frame;
